3807a9fe6df69820debc1e8e26c34e4424d7074a
xiztnt <chadolosky@gmail.com>
2025-03-05 22:15:26 -0500

    new stoof

--- app.js
+++ app.js
@@ -12,19 +12,6 @@ app.listen(port, () => {
   console.log(` server listening on port ${port} `)
 })
 
-//FETCH THEN GET
-
-// Provided URL: http://99.79.77.144:3000/api/agents
-
-// const AgentData = async () => {
-//   //INFO REQ
-//     const response = await fetch("http://99.79.77.144:3000/api/agents") 
-//     const data = await response.json()
-//     // conversion to JSON is to return usable data (precautionary)
-//     console.log(data) 
-//   }
-//   AgentData() 
-//  LINE 12  MEANS TO CALL THE FUNCTION
 
 //ROUTES!!!
 
@@ -42,7 +29,6 @@ res.send(`server running on port ${port} and in the ${env} environment`)
 }
 
 
-
 //Error Route
 const error = (req,res) => {
 const StatusCode = 404
@@ -52,26 +38,22 @@ res.status(StatusCode).send(`Sorry agent not found ${StatusCode}`)
 
 //Email List Route
 
-//Replacer
-
-// const replacer (jsagents)
-const emaillist = (req,res) => {
-let EmailOBJ = JSON.stringify(jsagents,["email"]);
-let FinalOBJEmail = jsagents.map
-//GET, I need a get request since I'm getting email
-//adding commas, basically taking information out of the json file utilizing the commas
-// let FinalOBJEmail = EmailOBJ.map(agent => agent.email);
-// res.status(statuscode) ex. 200, 404, etc.
-// look up different status codes
-// ref: Create, update, delete, read (logic)
-//creating a proper try-catch for this API
-console.log(EmailOBJ)
-}
-
 const getEmailList = (req,res) => {
 let email = datafile.jsagents.map(list => list.email).join(",")
 gi
 res.send(email);
+}
+
+//
+const contactus = async (req,res) => {
+//asyn to wait so it can fulfill apromise
+
+const first_name = req.body.first_name
+const last_name = req.body.last_name
+const message = req.body.message
+
+res.send(`Thank you for contacting us ${first_name}${last_name}, we will contact you shortly!`)
+console.log(message);
 }
 
 
@@ -95,5 +77,6 @@ app.get("/hello",hello)
 app.get("/status",status)
 app.get("/error",error)
 app.get("/email-list",getEmailList)
+app.post("/contact-us",contactus)   
 }
 RouteCaller(app)
